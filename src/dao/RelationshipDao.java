8e078635578f8bb1fbdd4a70bc0008bb88d1d2e5
ecjtuzwd1 <ecjtuzwd@aliyun.com>
2019-07-26 09:35:07 +0800

    zwd

--- src/dao/RelationshipDao.java
+++ src/dao/RelationshipDao.java
@@ -10,9 +10,9 @@ import java.util.List;
  * @time 2019/7/24 星期三 16:21
  */
 public interface RelationshipDao {
-    public boolean addRelationShip(String userName, RelationshipBean relationship);
-    public boolean delRelationShip(String userName, RelationshipBean relationship);
-    public boolean updateRelationShip(String userName, RelationshipBean relationship);
-    public List<RelationshipBean> selectAllRelationship(String username);
-    public List<RelationshipBean> selectAllRelationshipByStatus(String username,int status);//根据状态查看关系表（0关注、1拉黑）默认关注
+    public boolean addRelationShip(String staffName, RelationshipBean relationship);
+    public boolean delRelationShip(String staffName, RelationshipBean relationship);
+    public boolean updateRelationShip(String staffName, RelationshipBean relationship);
+    public List<RelationshipBean> selectAllRelationship(String staffName);
+    public List<RelationshipBean> selectAllRelationshipByStatus(String staffName,int status);//根据状态查看关系表（0关注、1拉黑）默认关注
 }
